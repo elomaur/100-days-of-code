6d0d921f8c70882da8d44e9cbd1a4f606335c92a
Elom <babyshyne307@hotmail.com>
2018-10-24 21:57:31 -0600

    Practicing Loops and Conditionals in C#

---
+++ Exercises/program.cs
@@ -0,0 +1,59 @@
+using System;
+
+namespace Treehouse.PracticeSession
+{
+    class Program
+    {
+        public static void Main()
+        {
+            // TODO Declare a boolean variable named "keepGoing"
+            // and assign it a value of "true". Use this variable to 
+            // know when to exit the while loop.
+					
+					bool keepGoing = true;
+
+            // TODO Define a while loop.
+            // Keep looping as long as the variable "keepGoing" has a value of "true".
+					
+					while(keepGoing)
+						
+                // TODO Prompt the user with the text "Enter a number:" 
+                // and assign their value to an "entry" string variable.
+                
+					{
+					  Console.Write("Enter a number: ");
+					string entry = Console.ReadLine();
+						
+						// TODO If the user entered the text "quit"...
+							if (entry == "quit")
+						{
+              // TODO Exit the program.
+							keepGoing = false;
+						}
+						
+						// TODO Else the user didn't enter the text "quit"...
+						else
+						{
+								// TODO Parse the user's entry to an integer
+								int number = int.Parse(entry);
+							
+								// TODO Square the user's provided number 
+                    // (i.e. multiply the number by itself).
+								number = number * number;
+							
+										// TODO Output the result.
+                    // Example: "The square of 2 is 4." or 
+                    // "2 multiplied by itself is equal to 4."
+								Console.WriteLine("The square of " + Console.ReadLine() + "is equal to" + entry);
+						}
+						
+							{
+								// TODO Output the text "Goodbye!" after exiting the loop.
+								Console.WriteLine("Goodbye");
+							}
+					}
+            
+            
+        }
+    }
+}
